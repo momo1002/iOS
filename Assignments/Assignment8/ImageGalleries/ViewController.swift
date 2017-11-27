e1c97831f58f47163319df4b521156919d89051f
momo1002 <momoko.nakada1002@gmail.com>
2017-11-27 10:38:11 -0800

    add Assignment8

---
+++ Assignments/Assignment8/ImageGalleries/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageGalleries
+//
+//  Created by Momoko Nakada on 2017-11-27.
+//  Copyright © 2017 Momoko Nakada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
